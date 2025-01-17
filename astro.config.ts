bb851016fcfc54fd14737f84e23aa05190e3e426
Jan Štefanča <janstefanca@seznam.cz>
2025-01-17 12:45:16 +0100

    Improved LaTex rendering

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 import react from '@astrojs/react';
 
 export default defineConfig({
   output: 'server',
 
-  adapter: vercelServerless({
+  adapter: vercel({
     imageService: true,
   }),
 
